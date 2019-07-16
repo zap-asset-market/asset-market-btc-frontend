cc30f2e990fcb24f2213200be5bc8296ac01c7ac
Alex <alpuga8@gmail.com>
2019-07-16 17:23:33 -0400

    Connected To Smart Contract Using Web3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import web3 from './web3.js';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AuxiliaryMarket from './AuxiliaryMarket.js';
+
+class App extends Component {
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+
+    const currentPrice = await AuxiliaryMarket.methods
+      .getCurrentPrice()
+      .call()
+      .toString();
+
+    console.log(currentPrice.toString());
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Asset Market</h1>
+        <Button onClick={this.onClick} variant='contained' color='primary'>
+          getCurrentPrice()
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/AuxiliaryMarket.js
@@ -0,0 +1,221 @@
+import web3 from './web3.js';
+
+const address = '0x90Cc8ff484fE2A1bABc5c100f96a4e5A53A84f21';
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'coordinator',
+    outputs: [
+      {
+        name: '',
+        type: 'address'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'auxiliaryMarketToken',
+    outputs: [
+      {
+        name: '',
+        type: 'address'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'mainMarket',
+    outputs: [
+      {
+        name: '',
+        type: 'address'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'zapToken',
+    outputs: [
+      {
+        name: '',
+        type: 'address'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    name: 'assetPrices',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'auxTokenPrice',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [
+      {
+        name: '_zapCoor',
+        type: 'address'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'random',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_quantity',
+        type: 'uint256'
+      }
+    ],
+    name: 'buy',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_quantity',
+        type: 'uint256'
+      }
+    ],
+    name: 'sell',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'getCurrentPrice',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_address',
+        type: 'address'
+      }
+    ],
+    name: 'getBalance',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: 'amount',
+        type: 'uint256'
+      }
+    ],
+    name: 'allocateZap',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_owner',
+        type: 'address'
+      }
+    ],
+    name: 'getAMTBalance',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
